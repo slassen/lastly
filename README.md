8d71d1ccaa1ff8f5a763ad4b499909574b8568b4
Scott Lassen <slassnpm@gmail.com>
2020-11-05 07:24:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# lastly
+lastly is a lightweight, no-dependency Node.js library sending a final error via POST to an absolute url.
+
+## How it works
+<!-- - Run `npm i -s lastly` in your project -->
+When your program encounters an uncaught exception lastly takes over. Lastly follows the same rules for error handling as `process.on('uncaughtException', error => {})` so it will only runtime errors. If you had an error in your syntax, for example, then you'll need to fix your code the hard way! Currently, lastly will only send a POST request formatted like a Slack message. It is important to mention that if you currently have the aforementioned `uncaughtException` process event set up you will need to wrap that inside of lastly's `callback` function.
+
+If you are sending to a Slack incoming webhook then setup is very simple. You can provide an optional callback if you want to do something with the error after lastly processes it.
+
+**It is important that lastly is configured before en error is thrown. For this reason I suggest you configure lastly as early as you can in your project**
+
+## Basic setup
+Lastly has only one required parameter, an absolute URI. It can optionally take a second parameter, a callback function, that will return the error that lastly processed.
+```js
+const lastly = require('lastly')
+lastly('https://hooks.slack.com/services/XXXXX/XXXXX/xxxxxxxxxx');
+```
+
+## Setup with callback
+```js
+const lastly = require('lastly')
+lastly('https://hooks.slack.com/services/XXXXX/XXXXX/xxxxxxxxxx', (err) => {
+  // Handle err
+  console.error(err);
+});
+```
---
+++ index.js
@@ -0,0 +1,91 @@
+const http = require('http');
+const https = require('https');
+
+const config = {
+  initialized: false,
+};
+
+const formatMessage = (error) => {
+  return JSON.stringify({
+    "text": "Lastly encountered an error."
+  });
+};
+
+process.on('uncaughtException', error => {
+  if (!config.initialized) {
+    return console.error('Lastly is not sending error because config was not properly initialized.');
+  }
+
+  try {
+    const message = formatMessage(error);
+
+    const {
+      url,
+      secure,
+      hostname,
+      path,
+      callback,
+    } = config;
+
+    const request = (secure ? https : http).request({
+      hostname,
+      port: secure ? 443 : 80,
+      path,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': message.length
+      }
+    }, (response) => {
+      console.log(`Lastly sent an error to ${url} and recieved status code: ${response.statusCode}`);
+      if (callback) callback(error);
+      process.exit(1);
+    });
+
+    request.on('error', error => {
+      console.error(`Lastly encountered an error sending to ${url}.`);
+      console.error(error);
+      process.exit(1);
+    });
+
+    request.write(message);
+    request.end();
+  } catch(err) {
+    console.error('Lastly failed to send error.', err);
+    process.exit(1);
+  }
+});
+
+module.exports = (url, callback = null) => {
+  try {
+    let error = false;
+    const urlPattern = /^http(s)?:\/\/([^\/]*)(.*)/
+    if (typeof url !== 'string') {
+      console.error(`url [${url}] is not a valid string.`);
+      error = true;
+    } else {
+      const patternMatch = url.match(urlPattern);
+      if (patternMatch && patternMatch.length === 4) {
+        config.secure = patternMatch[1] === 's';
+        config.hostname = patternMatch[2];
+        config.path = patternMatch[3] || '/';
+      } else {
+        console.error(`url [${url}] is not a valid url.`);
+        error = true;
+      }
+    }
+
+    if (callback !== null && typeof callback !== 'function') {
+      console.error(`callback [${callback}] is not a function.`);
+      error = true;
+    }
+
+    if (!error) {
+      config.initialized = true;
+      config.url = url;
+      config.callback = callback;
+    }
+  } catch (err) {
+    console.error('Lastly encountered an error initializing.');
+  }
+};
